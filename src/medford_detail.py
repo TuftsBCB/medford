64b0186b420cf9069bca29d5dee596cf17ad196d
Polina Shpilker <polina.shpilker@tufts.edu>
2021-11-03 18:08:13 -0400

    Add support for Macros; repair detail unittests

--- src/medford_detail.py
+++ src/medford_detail.py
@@ -9,7 +9,20 @@
 #   A Depth (the number of Major tokens in the instance)
 #   Data    (the data stored within the instance)
 
+from typing import Union, TypeVar
+
 class detail() :
+    #static
+    template_flag = "[...]"
+    macro_head = "`@"
+    macro_flag = "`@"
+    comment_head = "#"
+    comment_flag = "# "
+    detail_head = "@"
+
+    macro_dictionary = {}
+
+    #instance
     Major_Tokens = []
     Combined_Major_Token = ""
     Minor_Token = ""
@@ -26,16 +39,48 @@ class detail() :
         self.Data = data
 
     @classmethod
-    def FromLine(cls, line, lineno, prev_detail) :
+    def _clear_cache(cls) :
+        detail.macro_dictionary = {}
+
+    #TODO : For the love of all that is good in this world, split this up into different cases...
+    #           e.g. "handle_macro"?
+    @classmethod
+    def FromLine(cls, line: str, lineno: int, prev_detail: Union[None, 'detail']) -> tuple[bool, bool, 'detail'] :
+        """Generate a Detail object from a line, the line number, and the Detail generated directly previous.
+
+        :param line: A string to parse into a detail, from a MFD file.
+        :type line: str
+        :param lineno: The line number in the MFD file that 'line' came from.
+        :type lineno: int
+        :param prev_detail: The previous detail generated from this MEDFORD file, if applicable.
+        :type prev_detail: Detail|None
+        :raises ValueError: A Template marker ("[...]") is observed in the provided line.
+        :raises ValueError: A Macro name that has already been used is detected.
+        :raises ValueError: A Macro is referred to that has not yet been defined.
+        :raises ValueError: A catch-all error when we have no idea how to process the line provided.
+        :return: A 3-tuple describing : Is this data, is this NOVEL data, and the Detail object. The 'novel' data flag was added to accomodate multi-line details.
+        :rtype: Tuple(Bool, Bool, Detail)
+        """
         line = line.strip()
 
-        if line[0] != "#" :
-            if "[...]" in line :
+        if line[0] != detail.comment_head :
+            if detail.template_flag in line :
                 raise ValueError("ERROR: Line " + str(lineno) + \
                     " contains the template marker [...]. Please substitute this with actual data!" + \
                     "\n\tLINE: " + line)
-        if(line[0] == "#") :
+
+        if(line[0] == detail.comment_head) :
             return (False, False, None)
+
+        elif line[:2] == detail.macro_head :
+            macro_name, macro_body = line[2:].split(" ",1)
+            if macro_name in detail.macro_dictionary.keys() :
+                raise ValueError("ERROR: Line " + str(lineno) + \
+                    " tries to define macro " + macro_name + " to be '" + macro_body + "', but " + \
+                    macro_name + " is already defined!")
+            detail.macro_dictionary[macro_name] = macro_body
+            return (False, False, None)
+
         elif line[0] == "@" :
             tokens, body = str.split(line, " ", 1)
             tokens = str.replace(tokens, '@', "")
@@ -49,7 +94,27 @@ class detail() :
             major_tokens = tokens_list[0].split("_")
             data = body
 
+            if detail.macro_flag in data :
+                first_ind = data.find(detail.macro_flag)
+                whitespace_after = data.find(" ", first_ind)
+                if whitespace_after == -1 :
+                    whitespace_after = len(data)
+
+                found_macro_name = data[first_ind+2:whitespace_after]
+
+                if found_macro_name in detail.macro_dictionary.keys() :
+                    data = data[:first_ind] + detail.macro_dictionary[found_macro_name] + data[whitespace_after:]
+                else :
+                    raise ValueError("ERROR: Line " + str(lineno) + \
+                        " tries to use macro " + found_macro_name + ", but this macro has not previously been defined." + \
+                        " Are you sure you did not mis-spell the macro name? Or that you actually defined it?")
+
+            if detail.comment_flag in data :
+                comment_ind = data.find(detail.comment_flag) 
+                data = data[:comment_ind]
+
             return (True, True, cls(major_tokens, minor_token, lineno, depth, data))
+
         else :
             if prev_detail is None:
                 raise ValueError("ERROR: Line " + str(lineno) + \
--- src/test_detail.py
+++ src/test_detail.py
@@ -3,26 +3,63 @@ import unittest
 from medford_detail import *
 
 class TestDetailMethods(unittest.TestCase) :
+    def setUp(self) :
+        detail._clear_cache()
+
     def test_detail_simple(self) :
         example_line = "@Date 02/24"
-        d = detail.FromLine(example_line, -1)
+        is_detail, is_new, d = detail.FromLine(example_line, -1, None)
         self.assertListEqual(d.Major_Tokens, ["Date"])
         self.assertEqual(d.Minor_Token, "desc")
         self.assertEqual(d.Data, "02/24")
 
     def test_detail_ordinary(self) :
         example_line = "@Date-Note Samples Obtained"
-        d = detail.FromLine(example_line, -1)
+        is_detail, is_new, d = detail.FromLine(example_line, -1, None)
         self.assertListEqual(d.Major_Tokens, ["Date"])
         self.assertEqual(d.Minor_Token, "Note")
         self.assertEqual(d.Data, "Samples Obtained")
     
     def test_detail_2_level_recursive(self) :
         example_line = "@Freeform_Date-Note Samples Obtained"
-        d = detail.FromLine(example_line, -1)
+        is_detail, is_new, d = detail.FromLine(example_line, -1, None)
         self.assertListEqual(d.Major_Tokens, ["Freeform","Date"])
         self.assertEqual(d.Minor_Token, "Note")
         self.assertEqual(d.Data, "Samples Obtained")
+
+    # Tests for Macro functionality.
+    def test_add_macro(self) :
+        example_line = "`@macro_name macro body"
+        is_detail, is_new, d = detail.FromLine(example_line, -1, None)
+        self.assertListEqual(list(detail.macro_dictionary.keys()), ["macro_name"])
+
+    def test_add_multiple_macros(self) :
+        example_line = "`@macro_name macro body"
+        example_line_2 = "`@macro_name_2 macro body"
+        _, _, _ = detail.FromLine(example_line, -1, None)
+        _, _, _ = detail.FromLine(example_line_2, -1, None)
+        self.assertListEqual(list(detail.macro_dictionary.keys()), ["macro_name", "macro_name_2"])
+
+    def test_add_same_macro_name(self) :
+        example_line = "`@macro_name macro body"
+        example_line_2 = "`@macro_name macro body"
+        with self.assertRaises(ValueError) :
+            detail.FromLine(example_line, -1, None)
+            detail.FromLine(example_line_2, -1, None)
+    
+    def test_correctly_substitutes_macro(self) :
+        example_line = "`@macro_name macro body"
+        example_line_2 = "@major-minor text `@macro_name"
+        detail.FromLine(example_line, -1, None)
+        is_detail, is_new, d = detail.FromLine(example_line_2, -1, None)
+        self.assertEqual(d.Data, "text macro body")
+
+    def test_correctly_substitutes_macro_with_stuff_after(self) :
+        example_line = "`@macro_name macro body"
+        example_line_2 = "@major-minor text `@macro_name asdf asdf asdf"
+        detail.FromLine(example_line, -1, None)
+        is_detail, is_new, d = detail.FromLine(example_line_2, -1, None)
+        self.assertEqual(d.Data, "text macro body asdf asdf asdf")
 
 if __name__ == '__main__':
     unittest.main()
